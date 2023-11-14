929bde7348ff0371b41f2f5906557ec3387596ed
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2023-11-14 12:04:54 +0200

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component, Input } from '@angular/core';
+import { ValidateService } from './services/validate.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  @Input() value: string;
+
+  title = 'angular-validation';
+
+  inputValue: string;
+  validationMessage = 'Please enter your password';
+  passwordReliability = 'empty';
+  colors = {
+    gray: 'rgb(147, 142, 142)',
+    red: 'rgb(255, 21, 0)',
+    yellow: 'rgb(255, 255, 0)',
+    green: 'rgb(22, 183, 22)',
+  };
+
+  constructor(private validateService: ValidateService) {}
+
+  getInputValue(value: string) {
+    this.inputValue = value;
+    this.passwordReliability = this.validateService.validatePassword(
+      this.inputValue || ''
+    );
+  }
+
+  getValidationMessage() {
+    if (this.passwordReliability === 'short') {
+      return 'The password is too short';
+    }
+    if (this.passwordReliability === 'easy') {
+      return 'The password is too easy';
+    }
+    if (this.passwordReliability === 'medium') {
+      return 'The password isn`t very strong';
+    }
+    if (this.passwordReliability === 'strong') {
+      return 'This password strong';
+    }
+    return 'Enter your password';
+  }
+
+  getStyleForEasyPassword() {
+    if (this.passwordReliability === 'short') return this.colors.red;
+    if (
+      this.passwordReliability === 'easy' ||
+      this.passwordReliability === 'medium'
+    ) {
+      return this.colors.yellow;
+    }
+    if (this.passwordReliability === 'strong') return this.colors.green;
+    return this.colors.gray;
+  }
+
+  getStyleForMediumPassword() {
+    if (this.passwordReliability === 'short') return this.colors.red;
+    if (this.passwordReliability === 'medium') return this.colors.yellow;
+    if (this.passwordReliability === 'strong') return this.colors.green;
+    return this.colors.gray;
+  }
+  getStyleForStrongPassword() {
+    if (this.passwordReliability === 'short') return this.colors.red;
+    if (this.passwordReliability === 'strong') return this.colors.green;
+    return this.colors.gray;
+  }
+}
---
+++ src/app/services/validate.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValidateService {
+  private conditions = {
+    letters: /[a-zA-Z]/gm,
+    digits: /[0-9]/gm,
+    symbols: /[_\W]/gm,
+  };
+
+  constructor() {}
+
+  private matchValue(value: string): number | 'empty' {
+    let count = 0;
+    if (value === '') return 'empty';
+    if (value.length < 8) return 0;
+
+    Object.values(this.conditions).forEach(item => {
+      if (value.match(item)) {
+        count += 1;
+      }
+    });
+    return count;
+  }
+
+  validatePassword(value: string) {
+    const conditionsCount = this.matchValue(value);
+    let message: string;
+
+    switch (conditionsCount) {
+      case 'empty':
+        message = 'empty';
+        break;
+      case 0:
+        message = 'short';
+        break;
+      case 3:
+        message = 'strong';
+        break;
+      case 2:
+        message = 'medium';
+        break;
+      case 1:
+        message = 'easy';
+        break;
+    }
+    return message;
+  }
+}
