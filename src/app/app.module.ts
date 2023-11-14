0d33161693e844e8d7ed3b73ee08423eebc6edfa
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2023-11-14 22:04:33 +0200

    del input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
